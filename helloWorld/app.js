0e796446cc2bf37065bb105f31fde7e1ef9db927
Himanshu Bhardwaj <himanshu.bhardwaj307@gmail.com>
2024-05-08 23:51:26 +0530

    lesson 3

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "hello"),
-    React.createElement("h2", {}, "hello"),
-  ])
+
+const Title = () => <h1 className="head"> React using JSX</h1>;
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Functional Component</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
