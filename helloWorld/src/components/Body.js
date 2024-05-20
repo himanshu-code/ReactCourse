2ab811a3a6d939191689f35b0ef2e206c1e77162
Himanshu Bhardwaj <himanshu.bhardwaj307@gmail.com>
2024-05-20 23:56:44 +0530

    lesson 5

--- helloWorld/src/components/Body.js
+++ helloWorld/src/components/Body.js
@@ -1,11 +1,25 @@
 import RestrauntCard from "./Restraunt";
 import resObj from "../utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
+  const [resList, setresList] = useState(resObj);
+  const getTopRestraunt = () => {
+    console.log("filtering");
+    let filteredList = resList.filter((item) => {
+      return item.info.avgRating >= 4.2;
+    });
+    setresList(filteredList);
+  };
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={getTopRestraunt}>
+          Top Rated Restraunt{" "}
+        </button>
+      </div>
       <div className="res-container">
-        {resObj.map((res) => {
+        {resList.map((res) => {
           return <RestrauntCard resdata={res} key={res.info.id} />;
         })}
       </div>
